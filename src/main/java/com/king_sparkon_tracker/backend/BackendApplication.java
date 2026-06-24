8b4da116eec8e985c2aa8238b946ac4a86e3da31
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-06-24 09:09:10 +0200

    Move caching enablement out of root application

--- src/main/java/com/king_sparkon_tracker/backend/BackendApplication.java
+++ src/main/java/com/king_sparkon_tracker/backend/BackendApplication.java
@@ -2,11 +2,9 @@ package com.king_sparkon_tracker.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableCaching
 @EnableScheduling
 public class BackendApplication {
 
