1efdc46955d62e823c2ab42c0c4c084df47ce2e6
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-06-24 08:20:07 +0200

    Add feature and billing policy Redis caches

--- src/main/java/com/king_sparkon_tracker/backend/config/RedisCacheConfig.java
+++ src/main/java/com/king_sparkon_tracker/backend/config/RedisCacheConfig.java
@@ -17,6 +17,11 @@ public class RedisCacheConfig {
 	public static final String PRIVILEGES_CACHE = "privileges";
 	public static final String PRIVILEGE_BY_ROLE_CACHE = "privilegeByRole";
 	public static final String PROMOTION_QUOTES_CACHE = "promotionQuotes";
+	public static final String BILLING_PLANS_CACHE = "billingPlans";
+	public static final String AFFILIATE_COMMISSION_TIERS_CACHE = "affiliateCommissionTiers";
+	public static final String BUSINESS_PLAN_PRICES_CACHE = "businessPlanPrices";
+	public static final String BUSINESS_PLAN_WORKER_LIMITS_CACHE = "businessPlanWorkerLimits";
+	public static final String BUSINESS_FEATURE_ACCESS_CACHE = "businessFeatureAccess";
 
 	@Bean
 	RedisCacheConfiguration redisCacheConfiguration() {
@@ -32,7 +37,12 @@ public class RedisCacheConfig {
 		Map<String, RedisCacheConfiguration> cacheConfigurations = Map.of(
 				PRIVILEGES_CACHE, redisCacheConfiguration.entryTtl(Duration.ofHours(12)),
 				PRIVILEGE_BY_ROLE_CACHE, redisCacheConfiguration.entryTtl(Duration.ofHours(12)),
-				PROMOTION_QUOTES_CACHE, redisCacheConfiguration.entryTtl(Duration.ofMinutes(10)));
+				PROMOTION_QUOTES_CACHE, redisCacheConfiguration.entryTtl(Duration.ofMinutes(10)),
+				BILLING_PLANS_CACHE, redisCacheConfiguration.entryTtl(Duration.ofHours(6)),
+				AFFILIATE_COMMISSION_TIERS_CACHE, redisCacheConfiguration.entryTtl(Duration.ofHours(6)),
+				BUSINESS_PLAN_PRICES_CACHE, redisCacheConfiguration.entryTtl(Duration.ofHours(6)),
+				BUSINESS_PLAN_WORKER_LIMITS_CACHE, redisCacheConfiguration.entryTtl(Duration.ofHours(6)),
+				BUSINESS_FEATURE_ACCESS_CACHE, redisCacheConfiguration.entryTtl(Duration.ofMinutes(15)));
 
 		return builder -> builder.withInitialCacheConfigurations(cacheConfigurations);
 	}
