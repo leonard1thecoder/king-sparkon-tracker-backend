1eae5458c6205ac2576c216bd9cbd3684fa2cd26
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-06-29 16:57:52 +0200

    Fix Google Storage multipart upload build

--- src/main/java/com/king_sparkon_tracker/backend/config/GoogleStorageMultipartConfig.java
+++ src/main/java/com/king_sparkon_tracker/backend/config/GoogleStorageMultipartConfig.java
@@ -1,9 +1,7 @@
 package com.king_sparkon_tracker.backend.config;
 
-import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.unit.DataSize;
 
 import jakarta.servlet.MultipartConfigElement;
 
@@ -13,9 +11,7 @@ public class GoogleStorageMultipartConfig {
 	@Bean
 	MultipartConfigElement multipartConfigElement(GoogleStorageProperties properties) {
 		long maxBytes = Math.max(properties.getMaxFileSizeBytes(), 1L);
-		MultipartConfigFactory factory = new MultipartConfigFactory();
-		factory.setMaxFileSize(DataSize.ofBytes(maxBytes));
-		factory.setMaxRequestSize(DataSize.ofBytes(maxBytes + 1024L * 1024L));
-		return factory.createMultipartConfig();
+		long maxRequestBytes = maxBytes + 1024L * 1024L;
+		return new MultipartConfigElement("", maxBytes, maxRequestBytes, 0);
 	}
 }
