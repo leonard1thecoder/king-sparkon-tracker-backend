4b5985c8dd913d09d0377d5af8f8c19b384bab7c
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-06-29 20:57:27 +0200

    Make Redis cache manager primary

--- src/main/java/com/king_sparkon_tracker/backend/config/RedisCacheConfig.java
+++ src/main/java/com/king_sparkon_tracker/backend/config/RedisCacheConfig.java
@@ -7,6 +7,7 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
@@ -39,6 +40,7 @@ public class RedisCacheConfig {
 	}
 
 	@Bean(name = "cacheManager")
+	@Primary
 	CacheManager redisCacheManager(
 			RedisConnectionFactory redisConnectionFactory,
 			RedisCacheConfiguration redisCacheConfiguration) {
