5e2d5f86cef3f148b4390ed8a2cca237f604dc28
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-07-01 00:21:36 +0200

    feat: add job opportunity experience levels

---
+++ src/main/java/com/king_sparkon_tracker/backend/model/JobExperienceLevel.java
@@ -0,0 +1,11 @@
+package com.king_sparkon_tracker.backend.model;
+
+public enum JobExperienceLevel {
+	LESS_THAN_ONE_YEAR,
+	ONE_YEAR,
+	TWO_YEARS,
+	THREE_YEARS,
+	FOUR_YEARS,
+	FIVE_YEARS,
+	GREATER_THAN_FIVE_YEARS
+}
