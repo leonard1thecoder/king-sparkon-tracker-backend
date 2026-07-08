d6a9d7f22aa56cb69e5a858160c581e1ac99baf6
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-07-08 02:02:40 +0200

    Disable Flyway for H2 context load test

--- src/test/java/com/king_sparkon_tracker/backend/BackendApplicationTests.java
+++ src/test/java/com/king_sparkon_tracker/backend/BackendApplicationTests.java
@@ -3,7 +3,7 @@ package com.king_sparkon_tracker.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.flyway.enabled=false")
 class BackendApplicationTests {
 
 	@Test
