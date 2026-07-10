d356f257fba2dcf9b15470fc95c4248eaa50c487
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-07-10 13:02:03 +0200

    Return retryable Stripe webhook HTTP statuses

--- src/main/java/com/king_sparkon_tracker/backend/controller/StripeWebhookController.java
+++ src/main/java/com/king_sparkon_tracker/backend/controller/StripeWebhookController.java
@@ -1,5 +1,7 @@
 package com.king_sparkon_tracker.backend.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.king_sparkon_tracker.backend.dto.StripeWebhookResponse;
+import com.king_sparkon_tracker.backend.model.StripeWebhookProcessingStatus;
 import com.king_sparkon_tracker.backend.service.StripeWebhookService;
 
 @RestController
@@ -20,9 +23,16 @@ public class StripeWebhookController {
 	}
 
 	@PostMapping
-	public StripeWebhookResponse process(
+	public ResponseEntity<StripeWebhookResponse> process(
 			@RequestBody String rawPayload,
 			@RequestHeader(name = "Stripe-Signature", required = false) String stripeSignature) {
-		return stripeWebhookService.process(rawPayload, stripeSignature);
+		StripeWebhookResponse response = stripeWebhookService.process(rawPayload, stripeSignature);
+		if (response.status() == StripeWebhookProcessingStatus.SIGNATURE_FAILED) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+		}
+		if (response.status() == StripeWebhookProcessingStatus.FAILED) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+		}
+		return ResponseEntity.ok(response);
 	}
 }
