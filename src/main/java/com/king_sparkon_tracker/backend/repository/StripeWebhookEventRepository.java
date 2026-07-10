0607c172b52191bb711c45008fc3b9eb772e960f
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-07-10 12:59:44 +0200

    Find Stripe webhook event for retry

--- src/main/java/com/king_sparkon_tracker/backend/repository/StripeWebhookEventRepository.java
+++ src/main/java/com/king_sparkon_tracker/backend/repository/StripeWebhookEventRepository.java
@@ -1,5 +1,7 @@
 package com.king_sparkon_tracker.backend.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.king_sparkon_tracker.backend.model.StripeWebhookEvent;
@@ -7,4 +9,6 @@ import com.king_sparkon_tracker.backend.model.StripeWebhookEvent;
 public interface StripeWebhookEventRepository extends JpaRepository<StripeWebhookEvent, Long> {
 
 	boolean existsByStripeEventId(String stripeEventId);
+
+	Optional<StripeWebhookEvent> findByStripeEventId(String stripeEventId);
 }
