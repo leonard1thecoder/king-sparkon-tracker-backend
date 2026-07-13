ad35006395715efc23d7727e13611bd277dffe7a
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-07-13 16:19:16 +0200

    Extend business account revenue types

--- src/main/java/com/king_sparkon_tracker/backend/model/BusinessAccountEntryType.java
+++ src/main/java/com/king_sparkon_tracker/backend/model/BusinessAccountEntryType.java
@@ -2,6 +2,10 @@ package com.king_sparkon_tracker.backend.model;
 
 public enum BusinessAccountEntryType {
 	TOP_UP,
+	PRODUCT_SALE_CREDIT,
+	TICKET_SALE_CREDIT,
+	TIP_CREDIT,
 	PROMOTION_DEBIT,
-	TICKET_PROMOTION_DEBIT
+	TICKET_PROMOTION_DEBIT,
+	OWNER_WITHDRAWAL_DEBIT
 }
