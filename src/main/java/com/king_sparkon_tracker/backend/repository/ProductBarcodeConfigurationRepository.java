05e451f0d60ec77f9a6c1bda9e40d60c2431cbe6
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-07-31 10:52:44 +0200

    Add entity graph to findByProduct_Id method

--- src/main/java/com/king_sparkon_tracker/backend/repository/ProductBarcodeConfigurationRepository.java
+++ src/main/java/com/king_sparkon_tracker/backend/repository/ProductBarcodeConfigurationRepository.java
@@ -9,6 +9,9 @@ import com.king_sparkon_tracker.backend.model.ProductBarcodeConfiguration;
 
 public interface ProductBarcodeConfigurationRepository extends JpaRepository<ProductBarcodeConfiguration, Long> {
 
+@EntityGraph(attributePaths = "product")
+Optional<ProductBarcodeConfiguration> findByProduct_Id(Long productId);
+	
 	@EntityGraph(attributePaths = { "product", "product.business" })
 	List<ProductBarcodeConfiguration> findByProduct_Business_IdOrderByProduct_IdAsc(Long businessId);
 }
