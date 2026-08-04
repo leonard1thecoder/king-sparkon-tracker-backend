3caa652ac507ffcd04807427a03d3f5c439cdd20
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-08-04 13:23:26 +0200

    Add General ticket type and canonical method
    Add 'General' ticket type and canonical method for mapping.

--- src/main/java/com/king_sparkon_tracker/backend/tickets/model/TicketType.java
+++ src/main/java/com/king_sparkon_tracker/backend/tickets/model/TicketType.java
@@ -2,6 +2,11 @@ package com.king_sparkon_tracker.backend.tickets.model;
 
 public enum TicketType {
     REGULAR,
+    General,
     VIP,
     VVIP;
+
+    public TicketType canonical() {
+        return this == General ? REGULAR : this;
+    }
 }
