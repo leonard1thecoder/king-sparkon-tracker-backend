6e718654f475d7d5e1acccd78af1a6b2b14ec482
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-08-04 12:57:03 +0200

    Remove 'General' ticket type from TicketType enum
    Removed 'General' ticket type and its canonical method.

--- src/main/java/com/king_sparkon_tracker/backend/tickets/model/TicketType.java
+++ src/main/java/com/king_sparkon_tracker/backend/tickets/model/TicketType.java
@@ -2,11 +2,6 @@ package com.king_sparkon_tracker.backend.tickets.model;
 
 public enum TicketType {
     REGULAR,
-    General,
     VIP,
     VVIP;
-
-    public TicketType canonical() {
-        return this == General ? REGULAR : this;
-    }
 }
